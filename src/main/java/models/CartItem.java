38491dbd077adfc34c1f8f81adcfb0c6207f84e0
Silas Augusto <silasaugusto300@gmail.com>
2020-09-09 14:02:57 -0300

    minor changes

--- src/main/java/models/CartItem.java
+++ src/main/java/models/CartItem.java
@@ -8,32 +8,28 @@ public class CartItem {
   public CartItem() {}
 
   public CartItem(Product product, int quantity) {
+
     this.product = product;
     this.quantity = quantity;
   }
 
   public int getQuantity() {
-
     return quantity;
   }
 
   public void setQuantity(int quantity) {
-
     this.quantity = quantity;
   }
 
   public Product getProduct() {
-
     return product;
   }
 
   public void setProduct(Product product) {
-
     this.product = product;
   }
 
   public int calculateTotalCost() {
-
     return product.getPrice() * quantity;
   }
 }
--- src/main/java/models/ShoppingCart.java
+++ src/main/java/models/ShoppingCart.java
@@ -8,40 +8,38 @@ public class ShoppingCart {
   private List<CartItem> cartItemList;
 
   public ShoppingCart() {
-
     cartItemList = new ArrayList<>();
   }
 
   public ShoppingCart(List<CartItem> cartItemList) {
-
     this.cartItemList = cartItemList;
   }
 
   public int calculateTotalCost() {
+
     int summation = 0;
+
     for (CartItem item : cartItemList) {
+
       summation += item.calculateTotalCost();
     }
+
     return summation;
   }
 
   public void addItem(CartItem cartItem) {
-
     cartItemList.add(cartItem);
   }
 
   public void removeItem(CartItem cartItem) {
-
     cartItemList.remove(cartItem);
   }
 
   public void clearShoppingCart() {
-
     cartItemList.clear();
   }
 
   public List getItems() {
-
     return cartItemList;
   }
 }
