923f2f08971c44ecc985323fb9d9310cdfb08913
Silas Augusto <silasaugusto300@gmail.com>
2020-09-09 13:06:21 -0300

    Implementing model classes

---
+++ src/main/java/models/CartItem.java
@@ -0,0 +1,39 @@
+package main.java.models;
+
+public class CartItem {
+
+  private Product product;
+  private int quantity;
+
+  public CartItem() {}
+
+  public CartItem(Product product, int quantity) {
+    this.product = product;
+    this.quantity = quantity;
+  }
+
+  public int getQuantity() {
+
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+
+    this.quantity = quantity;
+  }
+
+  public Product getProduct() {
+
+    return product;
+  }
+
+  public void setProduct(Product product) {
+
+    this.product = product;
+  }
+
+  public int calculateTotalCost() {
+
+    return product.getPrice() * quantity;
+  }
+}
