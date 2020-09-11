c9cba38b93246c2c193174f8afd3b6ca7e6a42c7
Nelson Lins <54917418+NelsuLins@users.noreply.github.com>
2020-09-10 23:12:45 -0300

    Implementing model classes
    It contains the initial implementations of User and Pokemon models.

---
+++ src/main/java/models/Pokemon.java
@@ -0,0 +1,55 @@
+package main.java.models;
+
+public class Pokemon {
+
+  private String species;
+  private int life;
+  private int maxLife;
+  private PokemonType pokemonType;
+  private PokemonStatus pokemonStatus;
+  private Trainer trainer;
+
+  public Pokemon() {}
+
+  public Pokemon(String species, int life, int maxLife, PokemonType pokemonType, PokemonStatus pokemonStatus, Trainer trainer){
+
+    this.species = species;
+    this.life = life;
+    this.maxLife = maxLife;
+    this.pokemonType = pokemonType;
+    this.pokemonStatus = pokemonStatus;
+    this.trainer = trainer;
+  }
+
+  public String getSpecies() {
+    return species;
+  }
+
+  public int getLife() {
+    return life;
+  }
+
+  public int getMaxLife() {
+    return maxLife;
+  }
+
+  public PokemonType getType() {
+    return pokemonType;
+  }
+
+  public PokemonStatus getStatus() {
+    return pokemonStatus;
+  }
+
+  public Trainer getTrainer() {
+    return trainer;
+  }
+
+  public void setLife(int life) {
+    this.life = life;
+  }
+
+  public void setPokemonStatus(PokemonStatus pokemonStatus) {
+    this.pokemonStatus = pokemonStatus;
+  }
+}
