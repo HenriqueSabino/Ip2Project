64190bb3ccb4b0d2ebf5435abc20cb0a2659dbe5
Nelson Lins <54917418+NelsuLins@users.noreply.github.com>
2020-09-11 00:02:59 -0300

    Update User.java

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -2,47 +2,41 @@ package main.java.models;
 
 public abstract class User extends Person {
 
-  private String username;
-  private String password;
-  private String email;
+ private String username;
+ private String password;
+ private String email;
 
-  public User() {}
+ public User() {}
 
-  public User(
-      String name,
-      String birthCity,
-      String gender,
-      String username,
-      String password,
-      String email) {
+ public User(String name, String birthCity, String gender, String username, String password, String email) {
 
-    super(name, birthCity, gender);
-    this.username = username;
-    this.password = password;
-    this.email = email;
-  }
+   super(name, birthCity, gender);
+   this.username = username;
+   this.password = password;
+   this.email = email;
+ }
 
-  public String getUsername() {
-    return username;
-  }
+ public String getUsername() {
+   return username;
+ }
 
-  public String getPassword() {
-    return password;
-  }
+ public String getPassword() {
+   return password;
+ }
 
-  public String getEmail() {
-    return email;
-  }
+ public String getEmail() {
+   return email;
+ }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+ public void setUsername(String username) {
+   this.username = username;
+ }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+ public void setPassword(String password) {
+   this.password = password;
+ }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+ public void setEmail(String email) {
+   this.email = email;
+ }
 }
