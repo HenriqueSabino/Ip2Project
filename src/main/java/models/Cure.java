c9753562ebe8676ef802c133aeeed9fa4679cc4f
Nelson Lins <54917418+NelsuLins@users.noreply.github.com>
2020-10-08 16:16:00 -0300

    Implementing CureDao and Minor Changes in the Cure.java

--- src/main/java/models/Cure.java
+++ src/main/java/models/Cure.java
@@ -1,61 +1,111 @@
 package main.java.models;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class Cure {
+public class Cure implements Serializable {
 
-    private int id;
-    private LocalDateTime cureDate;
-    private Pokemon pokemon;
-    private int previousPokemonLife;
-    private PokemonStatus previousPokemonStatus;
-    private User employee;
-    private Trainer client;
+  private int id;
+  private LocalDateTime cureDate;
+  private Pokemon pokemon;
+  private int previousPokemonLife;
+  private PokemonStatus previousPokemonStatus;
+  private User employee;
+  private Trainer client;
 
-    public Cure(int id, LocalDateTime cureDate, Pokemon pokemon, int previousPokemonLife, PokemonStatus previousPokemonStatus, Trainer client) {
-        this.id = id;
-        this.cureDate = cureDate;
-        this.pokemon = pokemon;
-        this.previousPokemonLife = previousPokemonLife;
-        this.previousPokemonStatus = previousPokemonStatus;
-        this.client = client;
-    }
+  public Cure(
+      int id,
+      LocalDateTime cureDate,
+      Pokemon pokemon,
+      int previousPokemonLife,
+      PokemonStatus previousPokemonStatus,
+      Trainer client) {
+    this.id = id;
+    this.cureDate = cureDate;
+    this.pokemon = pokemon;
+    this.previousPokemonLife = previousPokemonLife;
+    this.previousPokemonStatus = previousPokemonStatus;
+    this.client = client;
+  }
 
-    public Cure(int id, LocalDateTime cureDate, Pokemon pokemon, int previousPokemonLife, PokemonStatus previousPokemonStatus, User employee, Trainer client) {
-        this.id = id;
-        this.cureDate = cureDate;
-        this.pokemon = pokemon;
-        this.previousPokemonLife = previousPokemonLife;
-        this.previousPokemonStatus = previousPokemonStatus;
-        this.employee = employee;
-        this.client = client;
-    }
+  public Cure(
+      int id,
+      LocalDateTime cureDate,
+      Pokemon pokemon,
+      int previousPokemonLife,
+      PokemonStatus previousPokemonStatus,
+      User employee,
+      Trainer client) {
+    this.id = id;
+    this.cureDate = cureDate;
+    this.pokemon = pokemon;
+    this.previousPokemonLife = previousPokemonLife;
+    this.previousPokemonStatus = previousPokemonStatus;
+    this.employee = employee;
+    this.client = client;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public Cure(
+      LocalDateTime cureDate,
+      Pokemon pokemon,
+      int previousPokemonLife,
+      PokemonStatus previousPokemonStatus,
+      User employee,
+      Trainer client) {
+    this.cureDate = cureDate;
+    this.pokemon = pokemon;
+    this.previousPokemonLife = previousPokemonLife;
+    this.previousPokemonStatus = previousPokemonStatus;
+    this.employee = employee;
+    this.client = client;
+  }
 
-    public LocalDateTime getCureDate() {
-        return cureDate;
-    }
+  public Cure(
+      LocalDateTime cureDate,
+      Pokemon pokemon,
+      int previousPokemonLife,
+      PokemonStatus previousPokemonStatus,
+      Trainer client) {
+    this.cureDate = cureDate;
+    this.pokemon = pokemon;
+    this.previousPokemonLife = previousPokemonLife;
+    this.previousPokemonStatus = previousPokemonStatus;
+    this.client = client;
+  }
 
-    public Pokemon getPokemon() {
-        return pokemon;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public int getPreviousPokemonLife() {
-        return previousPokemonLife;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public PokemonStatus getPreviousPokemonStatus() {
-        return previousPokemonStatus;
-    }
+  public LocalDateTime getCureDate() {
+    return cureDate;
+  }
 
-    public User getEmployee() {
-        return employee;
-    }
+  public void setCureDate(LocalDateTime cureDate) {
+    this.cureDate = cureDate;
+  }
 
-    public Trainer getClient() {
-        return client;
-    }
+  public Pokemon getPokemon() {
+    return pokemon;
+  }
+
+  public int getPreviousPokemonLife() {
+    return previousPokemonLife;
+  }
+
+  public PokemonStatus getPreviousPokemonStatus() {
+    return previousPokemonStatus;
+  }
+
+  public User getEmployee() {
+    return employee;
+  }
+
+  public Trainer getClient() {
+    return client;
+  }
 }
---
+++ src/main/java/models/dao/impl/exception/CureNotFoundException.java
@@ -0,0 +1,8 @@
+package main.java.models.dao.impl.exception;
+
+public class CureNotFoundException extends RuntimeException {
+
+  public CureNotFoundException(String message) {
+    super(message);
+  }
+}
